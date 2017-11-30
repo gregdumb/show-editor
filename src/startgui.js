cf5b9edb1f56b6dc1ba79e994745c8a9120b366b
Greg <gregoryjjb@gmail.com>
2017-11-30 13:43:34 -0800

    Bugfix - Removed Z

--- src/startgui.js
+++ src/startgui.js
@@ -2,7 +2,7 @@
 
 
 // Check to see if we have a server
-var modeRequest = $.get(API_PATH + "getprojectsZ.php");
+var modeRequest = $.get(API_PATH + "getprojects.php");
 
 // Remote 
 modeRequest.success(function(result) {
